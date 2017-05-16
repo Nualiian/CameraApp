382cb12ae61b6508db55a78e7a872e62e361febf
Richard Rosko <rosko.richard@gmail.com>
2017-05-16 19:05:46 +0200

    changes

--- index.ios.js
+++ index.ios.js
@@ -1,14 +1,17 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, Image, Button, TouchableHighlight } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, Image, Button, TouchableHighlight, NativeModules } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-
 import styles from './styles/styles.js';
 
 import SettingsScreen from './screens/SettingsScreen.js';
 import HomeScreen from './screens/HomeScreen.js';
 import CameraScreen from './screens/CameraScreen.js';
+
+import Camera from 'react-native-openalpr';
+const CameraManager = NativeModules.ALPRCameraManager;
+console.log(CameraManager);
 
 const CameraApp = StackNavigator({
 	Home: { screen: HomeScreen },
